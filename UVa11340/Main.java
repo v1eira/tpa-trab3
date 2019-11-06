6c46466f25002be47cd6c8e2075a498602aa1a64
v1eira <ewersonv@gmail.com>
2019-11-06 20:41:04 -0300

    fix UVa 11340 (Accepted)

--- UVa11340/Main.java
+++ UVa11340/Main.java
@@ -1,38 +1,44 @@
 import java.util.Scanner;
 
 public class Main {
+    static final int max = 128;
     public static void main(String[] args) {
         Scanner read = new Scanner(System.in);
+        int[] cents = new int[max];
+
         int n = read.nextInt();
 
         for (int i = 0; i < n; i++) {
-            float payment = 0;
+            int payment = 0;
             int k = read.nextInt();
-            String[][] paidCharacters = new String[k][2];
-            
+
+            for (int j = 0; j < max; j++) {
+                cents[j] = -1;
+            }
 
             for (int j = 0; j < k; j++) {
-                paidCharacters[j][0] = read.next();
-                paidCharacters[j][1] = read.next();
+                int character = (int) read.next().charAt(0);
+                int value = read.nextInt();
+                cents[character] = value;
             }
 
             int m = read.nextInt();
-
-            for (int x = 0; x <= m; x++) {
+            read.nextLine();
+            
+            for (int x = 0; x < m; x++) {
                 String line = read.nextLine();
                 for (int y = 0; y < line.length(); y++) {
-                    String character = Character.toString(line.charAt(y));
-                    for (int z = 0; z < paidCharacters.length; z++) {
-                        if (character.equals(paidCharacters[z][0])) {
-                            payment = payment + Integer.parseInt(paidCharacters[z][1]);
-                            break;
-                        }
-                    }
+                    int character = (int) line.charAt(y);
+                    if (character < 0 || character >= max || cents[character] < 0)
+                        payment += 0;
+                    else
+                        payment += cents[character];
                 }
             }
-            payment = payment / 100;
-            String money = String.format(java.util.Locale.US,"%.2f", payment);
-            System.out.println(money + "$");
+            if (payment % 100 < 10)
+                System.out.printf("%d.0%d$\n", payment / 100, payment % 100);
+            else
+                System.out.printf("%d.%d$\n", payment / 100, payment % 100);
         }
 
         read.close();
