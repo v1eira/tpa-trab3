73e670cb76d4fc81fddd649758635172e2965a49
v1eira <ewersonv@gmail.com>
2019-12-01 20:11:47 -0300

    fix UVa 00939 (Accepted)

--- uva00939/Main.java
+++ uva00939/Main.java
@@ -1,5 +1,6 @@
 /*
 UVa 00939 - Genes
+Autores: Ewerson Vieira Nascimento e Icaro Gavazza
 */
 
 import java.util.Scanner;
@@ -16,6 +17,7 @@ public class Main {
 
         String firstWord, secondWord;
         ArrayList<Person> people = new ArrayList<>();
+        int parents = 0;
 
         for (int i = 0; i < N; i++) {
             firstWord = read.next();
@@ -24,23 +26,38 @@ public class Main {
             if (secondWord.equals("dominant") || secondWord.equals("recessive") || secondWord.equals("non-existent")) {
                 Person p = new Person(firstWord, secondWord);
                 people.add(p);
+                parents += 1;
             }
             else {
                 int index = personIsIn(people, secondWord);
-                if (index >= 0) {
-                    Person p = people.get(index);
-                    p.parents[1][0] = firstWord;
-                    p.parents[1][1] = getPersonGene(people, firstWord);
-                    p.getGene();
-                }
+                if (index >= 0)
+                    people.get(index).parents[1][0] = firstWord;
                 else {
                     Person p = new Person(secondWord);
                     p.parents[0][0] = firstWord;
-                    p.parents[0][1] = getPersonGene(people, firstWord);
                     people.add(p);
                 }
             }
         }
+
+        int count = 0;
+
+        do {
+            for (int i = 0; i < people.size(); i++) {
+                Person p = people.get(i);
+                if (p.parents[0][0] != null && ((p.parents[0][1] == null || p.parents[0][1].equals("none")) || (p.parents[1][1] == null || p.parents[1][1].equals("none")))) {
+                    p.parents[0][1] = getPersonGene(people, p.parents[0][0]);
+                    p.parents[1][1] = getPersonGene(people, p.parents[1][0]);
+                    
+                    if (p.parents[0][1].equals("none") || p.parents[1][1].equals("none"))
+                        continue;
+                    else {
+                        p.getGene();
+                        count += 1;
+                    }
+                }
+            }
+        } while (count < (people.size() - parents));
 
         read.close();
 
@@ -53,27 +70,34 @@ public class Main {
 
     private static class Person implements Comparable<Person> {
         public String name;
-        public String gene = "";
-        public String[][] parents;
+        public String gene = "none";
+        public String[][] parents = new String[2][2];
 
         public Person (String name) {
             this.name = name;
-            this.parents = new String[2][2];
+            this.parents[0][0] = null;
+            this.parents[0][1] = null;
+            this.parents[1][0] = null;
+            this.parents[1][1] = null;
         }
 
         public Person (String name, String gene) {
             this.name = name;
             this.gene = gene;
+            this.parents[0][0] = null;
+            this.parents[0][1] = null;
+            this.parents[1][0] = null;
+            this.parents[1][1] = null;
         }
 
         public String getGene() {
-            if (this.gene.equals("") && this.parents.length == 2) {
+            if (this.gene.equals("none") && this.parents.length == 2) {
                 int dominant = 0;
                 int recessive = 0;
                 for (int i = 0; i < 2; i++) {
-                    if (this.parents[i][1] == "dominant")
+                    if (this.parents[i][1].equals("dominant"))
                         dominant += 1;
-                    if (this.parents[i][1] == "recessive")
+                    if (this.parents[i][1].equals("recessive"))
                         recessive += 1;
                 }
                 if (dominant == 2 || (dominant == 1 && recessive == 1))
@@ -94,15 +118,15 @@ public class Main {
 
     public static String getPersonGene(ArrayList<Person> pl, String name) {
         for (int i = 0; i < pl.size(); i++) {
-            if (pl.get(i).name == name)
+            if (pl.get(i).name.equals(name))
                 return pl.get(i).gene;
         }
-        return "";
+        return "none";
     }
 
     public static int personIsIn(ArrayList<Person> pl, String name) {
         for (int i = 0; i < pl.size(); i++) {
-            if (pl.get(i).name == name)
+            if (pl.get(i).name.equals(name))
                 return i;
         }
         return -1;
